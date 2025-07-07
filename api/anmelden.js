96ed854361159287031cbc85fadc7bf185685507
jbvvb <tomtroschuetz@gmail.com>
2025-07-07 22:50:08 +0200

    nah22

--- api/anmelden.js
+++ api/anmelden.js
@@ -1,4 +1,4 @@
-import { db } from "@/lib/firebaseAdmin";
+import { db } from "../../lib/firebaseAdmin";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).end();
